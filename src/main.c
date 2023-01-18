2c82b29bd6424d02e9f52d22f48eb5b288a32384
LucaOverflow <little.luca98@googlemail.com>
2023-01-18 23:11:45 +0100

    roughly removed unneeded stuff

--- src/main.c
+++ src/main.c
@@ -17,7 +17,6 @@
 #include <zigbee/zigbee_error_handler.h>
 #include <zigbee/zigbee_app_utils.h>
 #include <zb_nrf_platform.h>
-#include "zb_range_extender.h"
 
 
 /* Device endpoint, used to receive ZCL commands. */
@@ -26,13 +25,7 @@
 /* Type of power sources available for the device.
  * For possible values see section 3.2.2.2.8 of ZCL specification.
  */
-#define TEMPLATE_INIT_BASIC_POWER_SOURCE    ZB_ZCL_BASIC_POWER_SOURCE_DC_SOURCE
-
-/* LED indicating that device successfully joined Zigbee network. */
-#define ZIGBEE_NETWORK_STATE_LED            DK_LED3
-
-/* LED used for device identification. */
-#define IDENTIFY_LED                        DK_LED4
+#define TEMPLATE_INIT_BASIC_POWER_SOURCE    ZB_ZCL_BASIC_POWER_SOURCE_BATTERY
 
 /* Button used to enter the Identify mode. */
 #define IDENTIFY_MODE_BUTTON                DK_BTN4_MSK
@@ -62,21 +55,6 @@ ZB_ZCL_DECLARE_BASIC_ATTRIB_LIST(
 	&dev_ctx.basic_attr.zcl_version,
 	&dev_ctx.basic_attr.power_source);
 
-ZB_DECLARE_RANGE_EXTENDER_CLUSTER_LIST(
-	app_template_clusters,
-	basic_attr_list,
-	identify_attr_list);
-
-ZB_DECLARE_RANGE_EXTENDER_EP(
-	app_template_ep,
-	APP_TEMPLATE_ENDPOINT,
-	app_template_clusters);
-
-ZBOSS_DECLARE_DEVICE_CTX_1_EP(
-	app_template_ctx,
-	app_template_ep);
-
-
 /**@brief Function for initializing all clusters attributes. */
 static void app_clusters_attr_init(void)
 {
@@ -87,38 +65,6 @@ static void app_clusters_attr_init(void)
 	/* Identify cluster attributes data. */
 	dev_ctx.identify_attr.identify_time =
 		ZB_ZCL_IDENTIFY_IDENTIFY_TIME_DEFAULT_VALUE;
-}
-
-/**@brief Function to toggle the identify LED
- *
- * @param  bufid  Unused parameter, required by ZBOSS scheduler API.
- */
-static void toggle_identify_led(zb_bufid_t bufid)
-{
-	static int blink_status;
-
-	dk_set_led(IDENTIFY_LED, (++blink_status) % 2);
-	ZB_SCHEDULE_APP_ALARM(toggle_identify_led, bufid, ZB_MILLISECONDS_TO_BEACON_INTERVAL(100));
-}
-
-/**@brief Function to handle identify notification events on the first endpoint.
- *
- * @param  bufid  Unused parameter, required by ZBOSS scheduler API.
- */
-static void identify_cb(zb_bufid_t bufid)
-{
-	zb_ret_t zb_err_code;
-
-	if (bufid) {
-		/* Schedule a self-scheduling function that will toggle the LED */
-		ZB_SCHEDULE_APP_CALLBACK(toggle_identify_led, bufid);
-	} else {
-		/* Cancel the toggling function alarm and turn off LED */
-		zb_err_code = ZB_SCHEDULE_APP_ALARM_CANCEL(toggle_identify_led, ZB_ALARM_ANY_PARAM);
-		ZVUNUSED(zb_err_code);
-
-		dk_set_led(IDENTIFY_LED, 0);
-	}
 }
 
 /**@brief Starts identifying the device.
@@ -192,11 +138,6 @@ static void configure_gpio(void)
 	if (err) {
 		LOG_ERR("Cannot init buttons (err: %d)", err);
 	}
-
-	err = dk_leds_init();
-	if (err) {
-		LOG_ERR("Cannot init LEDs (err: %d)", err);
-	}
 }
 
 /**@brief Zigbee stack event handler.
@@ -206,9 +147,6 @@ static void configure_gpio(void)
  */
 void zboss_signal_handler(zb_bufid_t bufid)
 {
-	/* Update network status LED. */
-	zigbee_led_status_update(bufid, ZIGBEE_NETWORK_STATE_LED);
-
 	/* No application-specific behavior is required.
 	 * Call default signal handler.
 	 */
@@ -230,13 +168,7 @@ void main(void)
 	configure_gpio();
 	register_factory_reset_button(FACTORY_RESET_BUTTON);
 
-	/* Register device context (endpoints). */
-	ZB_AF_REGISTER_DEVICE_CTX(&app_template_ctx);
-
 	app_clusters_attr_init();
-
-	/* Register handlers to identify notifications */
-	ZB_AF_SET_IDENTIFY_NOTIFICATION_HANDLER(APP_TEMPLATE_ENDPOINT, identify_cb);
 
 	/* Start Zigbee default thread */
 	zigbee_enable();
